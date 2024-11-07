f33fa1c3de65b80602b0324f0e61d49b76fbadf4
lukvmil <millerluke1364@gmail.com>
2024-11-07 18:25:37 +0800

    sorting messages by publish time for dispatching'

--- dispatch_messages.py
+++ dispatch_messages.py
@@ -4,6 +4,7 @@ from app import orchestrator
 from app.dereference import deref
 import json, time
 
+messages = []
 with open("backfill.json", "r") as f:
     data = json.load(f)
     
@@ -11,19 +12,22 @@ with open("backfill.json", "r") as f:
         if channel["observed_messages"] == 0:
             continue
         
-        for message_str in channel["messages"]:
-            message = RID.from_string(message_str)
-            message_data = deref(message)
-            
-            author_user_id = message_data["user"]
-            
-            for reaction in message_data["reactions"]:
-                if reaction["name"] == "telescope":
-                    tagger_user_id = reaction["users"][0]
-                    break
-            
-            author = SlackUser(message.workspace_id, author_user_id)
-            tagger = SlackUser(message.workspace_id, tagger_user_id)
-                        
-            orchestrator.create_request_interaction(message, author, tagger)
-            time.sleep(1)
+        messages.extend([RID.from_string(msg_str) for msg_str in channel["messages"]])
+        
+        
+messages.sort(key=lambda x: x.message_id)
+for message in messages:
+    message_data = deref(message)
+    
+    author_user_id = message_data["user"]
+    
+    for reaction in message_data["reactions"]:
+        if reaction["name"] == "telescope":
+            tagger_user_id = reaction["users"][0]
+            break
+    
+    author = SlackUser(message.workspace_id, author_user_id)
+    tagger = SlackUser(message.workspace_id, tagger_user_id)
+                
+    orchestrator.create_request_interaction(message, author, tagger)
+    time.sleep(1)

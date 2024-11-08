4a2a69a668de0f2e600a702a0c2826eaa0363d55
broco347 <48035494+broco347@users.noreply.github.com>
2024-11-08 14:40:33 +1000

    Changed welcome and retract_success messages

--- app/messages.py
+++ app/messages.py
@@ -1,6 +1,22 @@
-consent_ui_welcome = "Welcome to the Observatory! You are receiving this notice because one of your messages has been tagged for observation. Your decision to opt in or out will apply to this message and all future messages observed by Telescope. If you chose to opt in, you will be informed when new messages are observed and have the option to retract them."
+consent_ui_welcome = """Greetings from the KOI Ethnography Research Team! We are exploring how Metagov uses and governs its Knowledge Organization Infrastructure (or KOI). You can learn more about our research here. You are receiving this notice because one of your messages has been tagged for observation.
+
+We’d like your permission to quote you in our research. Here’s how it works:
+- Your decision to opt in or out will apply to this message and all future messages observed by Telescope. 
+- If you opt in, you will be notified whenever new messages are observed, and you will have the option to retract them. Retracted messages will be removed from the ethnographic dataset.
+- Messages can be automatically retracted via the Telescope bot for up to four weeks after being added to the ethnographic dataset. After this period, individuals must contact @Brooke Ann Coco directly via DM to retract messages.
+- You may withdraw your consent entirely at any time, in which case none of your posts will be quoted in future research publications. 
+- Additional information regarding the role and rights of ethnographic research participants can be found here.
+
+Note: This consent is for the ethnographic research only. Metagov has a separate data use policy regarding its use of Slack messages.
+
+Do you consent to participate in the research and allow your tagged posts to be included in the ethnographic data?
+
+A) ✅ Opt in: Yes, you may quote my posts and attribute them to my Slack Handle.
+B) ⚠️ Opt in (anonymously): Yes, you may quote my posts anonymously, but do not use my Slack handle or any other identifying information.
+C) ❌ Opt out: No, you may not quote my posts in your research."""
+
 consent_ui_msg_header = "Your message has been tagged for observation!"
 retract_ui_msg_header = "Your message has been observed by Telescope!"
-retract_success = "Your message has been retracted successfully"
-retract_failure = "The retraction period has passed, please contact researchers directly if you still wish to retract or anonymize your message"
-anonymize_success = "Your message has been anonymized successfully"
+retract_success = "Your message has successful been retracted and withdrawn from the ethnographic dataset."
+retract_failure = "The retraction period has passed, please contact researchers directly if you still wish to retract or anonymize your message."
+anonymize_success = "Your message has been anonymized successfully."

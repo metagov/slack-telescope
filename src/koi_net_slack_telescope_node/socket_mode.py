d6983897906c2585187d9bc99129e5ef11f786c3
lukvmil <millerluke1364@gmail.com>
2026-02-09 11:09:35 -0500

    fixed issue prevent proper restarting of slack telescope due to reused socketmodehandler

--- src/koi_net_slack_telescope_node/socket_mode.py
+++ src/koi_net_slack_telescope_node/socket_mode.py
@@ -1,14 +1,19 @@
+from dataclasses import dataclass
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 from .config import SlackTelescopeNodeConfig
 
 
+@dataclass
 class SlackSocketMode:
-    def __init__(self, slack_app: App, config: SlackTelescopeNodeConfig):
-        self.handler = SocketModeHandler(slack_app, config.env.slack_app_token)
+    slack_app: App
+    config: SlackTelescopeNodeConfig
     
     def start(self):
+        self.handler = SocketModeHandler(
+            app=self.slack_app, 
+            app_token=self.config.env.slack_app_token)
         self.handler.connect()
         
     def stop(self):

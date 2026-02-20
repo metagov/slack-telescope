45cfee216404bfca1ff138df5abe43c86d5a3fc8
lukvmil <millerluke1364@gmail.com>
2026-02-20 19:57:57 +0000

    removed reference to blockscience

--- src/koi_net_slack_telescope_node/message_content.py
+++ src/koi_net_slack_telescope_node/message_content.py
@@ -1,12 +1,10 @@
-consent_ui_welcome = """Greetings from the KOI Ethnography Research Team! We are exploring how BlockScience uses and governs its Knowledge Organization Infrastructure (or KOI). You are receiving this notice because one of your messages has been tagged for observation.
+consent_ui_welcome = """Greetings from the KOI Ethnography Research Team! We are exploring how Knowledge Organization Infrastructure (or KOI) is used and governed. You are receiving this notice because one of your messages has been tagged for observation.
 
 We’d like your permission to quote you in our research. Here’s how it works:
  - *Your decision to opt in or out will apply to this message and all future messages observed by Telescope.* 
  - If you opt in, *you will be notified whenever new messages are observed,* and you will have the option to retract them. Retracted messages will be removed from the ethnographic dataset.
  - Messages can be *automatically retracted via the Telescope bot for up to four weeks* after being added to the ethnographic dataset. After this period, individuals must contact the researchers directly via DM to retract messages.
  - You may *withdraw your consent entirely at any time,* in which case none of your posts will be quoted in future research publications. 
- 
-Additional information regarding the role and rights of ethnographic research participants can be found <https://blockscienceteam.slack.com/files/U04PMMHGERJ/F08U18C898U/hre_adms__picf_individuals__5_.pdf|here>.
 
 
 *Note:* This consent is for the ethnographic research only.
